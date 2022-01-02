72e2f7becaca8cea9814ac8721a1969c831b3858
I-am-deepak <9452089615dk@gmail.com>
2022-01-02 23:58:53 +0530

    learning python basics

--- git demo.py
+++ git demo.py
@@ -1,3 +1,33 @@
-print("hello world")
-dict=[7,7]
-print(sum(dict))
+#print('hello world')
+#print('hello', end=" ")
+#print("world")
+#print(2**7)
+#print(5,7,9,'deep', sep='-')
+
+#import keyword
+#print(keyword.kwlist)
+#print(keyword.iskeyword('else'))
+
+#print(3 and 7 and 0)
+#print(6 and 5 and 8)
+
+#print('large' and ' ' and 'small')
+
+#print(len(str(2**100000)))
+#a=1101
+#print(0b101)
+#print(bin(a))
+#print(hex(a))
+#print(oct(a))
+print(int('657',8))
+print(int(8))
+print(7.9.hex())
+import math
+print(math.pi)
+print(math.sqrt(78))
+from math import factorial, sqrt
+a=6
+b=9
+print(sqrt(a)/factorial(b))
+s='pap'
+print(s==s[::-1])
